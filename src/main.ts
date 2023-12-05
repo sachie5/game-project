c1f04b03af67007df1b7a578612d84529e808743
sachie5 <sachamccormack@gmail.com>
2023-12-05 21:15:09 +0000

    edited styling so more proportionate

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,11 @@ import { mathQuestions } from "./quizData";
 
 const startPage = document.querySelector<HTMLElement>(".quiz__container");
 const startButton = document.querySelector<HTMLButtonElement>("#start");
-const playerScoreInput =
-  document.querySelector<HTMLHeadingElement>(".header__player");
-const questionPage =
-  document.querySelector<HTMLDivElement>(".pages__container");
-const questionCategory = document.querySelector<HTMLHeadingElement>(
-  ".questions--category"
-);
-const question = document.querySelector<HTMLHeadingElement>(
-  ".questions--question"
-);
-const answers = document.querySelectorAll<HTMLButtonElement>(
-  ".answers__container--options"
-);
+const playerScoreInput =  document.querySelector<HTMLHeadingElement>(".header__player");
+const questionPage =  document.querySelector<HTMLDivElement>(".pages__container");
+const questionCategory = document.querySelector<HTMLHeadingElement>(".questions--category");
+const question = document.querySelector<HTMLHeadingElement>(".questions--question");
+const answers = document.querySelectorAll<HTMLButtonElement>(".answers__container--options");
 const homeButton = document.querySelector<HTMLButtonElement>(".header__start");
 const quizTimer = document.querySelector<HTMLParagraphElement>(".quiz__timer");
 const grammarCategory = document.querySelector<HTMLButtonElement>("#grammar");
@@ -40,26 +32,45 @@ if (
   throw new Error("Issue with selector");
 }
 
-const chooseCategory = () =>{
+const chooseCategory = () => {
   startPage.style.display = "none";
   categoryPage.style.display = "flex";
-}
+};
 
-startButton.addEventListener("click", chooseCategory)
 
 //Global variables
 let score: number = 0;
-let questionIndex: number = 0 /*  Math.floor(Math.random() * 5) */;
+let questionIndex: number = 0; /*  Math.floor(Math.random() * 5) */
 let counter: number = 15;
 let timeStop: boolean = false;
 let timer: number = 0;
 let questionsArr: Quiz[] = [];
 
+//Timer function
+const countDown = () => {
+  if (!timeStop) {
+    timer = setInterval(() => {
+      quizTimer.innerHTML = `${counter}`;
+      counter--;
+      if (counter < 0) {
+        clearInterval(timer);
+        answers.forEach((answer) => {
+          answer.disabled = true;
+        });
+        timeStop = true;
+        return;
+      }
+    }, 1000);
+  } else {
+    clearInterval(timer);
+    return;
+  }
+};
 
-//Start Quiz
+//Start Quiz function
 const startQuiz = (event: Event) => {
   const buttonClicked = event.currentTarget as HTMLButtonElement;
-  if(buttonClicked.innerText === "Grammar"){
+  if (buttonClicked.innerText === "Grammar") {
     questionsArr = quizQuestions;
   } else {
     questionsArr = mathQuestions;
@@ -67,8 +78,8 @@ const startQuiz = (event: Event) => {
 
   //Score
   playerScoreInput.innerHTML += `<span class= score> ${score}</span>`;
-  
-  //Timer  
+
+  //Timer
   counter = 15;
   timeStop = false;
   countDown();
@@ -78,85 +89,62 @@ const startQuiz = (event: Event) => {
   questionPage.style.display = "flex";
   questionCategory.innerText = `${questionsArr[questionIndex].category}`;
   question.innerText = `${questionsArr[questionIndex].question}`;
-  let currentQuestion = questionsArr[questionIndex].answers;
-
+  const currentQuestion = questionsArr[questionIndex].answers;
 
   answers.forEach((answer, i) => {
+    answer.disabled = false;
     answer.innerText = `${currentQuestion[i].answer}`;
   });
-
 };
 
-grammarCategory.addEventListener("click", startQuiz);
-mathsCategory.addEventListener("click", startQuiz);
 
-
-//Timer 
-const countDown = () => { 
- if(!timeStop) {
-  timer = setInterval(() => {
-   quizTimer.innerHTML = `${counter}`;
-    counter--;
-    if (counter < 0) {
-      clearInterval(timer);
-      answers.forEach(answer => {
-        answer.disabled = true;
-      })
-      timeStop = true; 
-    }
-  }, 1000);
-} else {
-  clearInterval(timer);
-}
-}
-
-//Checking answers
+//Checking answers function
 const checkAnswers = (event: Event) => {
   const buttonClicked = event.target as HTMLButtonElement;
   let answersArray = questionsArr[questionIndex].answers;
-   for (let index = 0; index < answersArray.length; index++) {
-      if (buttonClicked.innerText === answersArray[index].answer) {
-        if(answersArray[index].correctIncorrect === "Correct"){
-      console.log(answersArray[index].correctIncorrect);
-      buttonClicked.style.backgroundColor = "green";
-      playerScoreInput.innerText = `Player score:  ${(score + 1)}`;
-      //Timer reset
-      timeStop = true;
-      answers.forEach(answer => {
-        answer.disabled = true;
-      })
-    } else {
-      buttonClicked.style.backgroundColor = "red";
-      //Timer reset
-      timeStop = true;
-      answers.forEach(answer => {
-        answer.disabled = true;
-      })
+  for (let index = 0; index < answersArray.length; index++) {
+    if (buttonClicked.innerText === answersArray[index].answer) {
+      if (answersArray[index].correctIncorrect === "Correct") {
+        console.log(answersArray[index].correctIncorrect);
+        buttonClicked.style.backgroundColor = "green";
+        playerScoreInput.innerText = `Player score:  ${score + 1}`;
+        //Timer reset
+        timeStop = true;
+        answers.forEach((answer) => {
+          answer.disabled = true;
+        });
+        clearInterval(timer);
+      } else {
+        buttonClicked.style.backgroundColor = "red";
+        //Timer reset
+        clearInterval(timer);
+        answers.forEach((answer) => {
+          answer.disabled = true;
+        });
+      }
     }
   }
-}
-}
+};
 
 answers.forEach((answer) => {
-  answer.addEventListener("click", checkAnswers)
+  answer.addEventListener("click", checkAnswers);
 });
 
-//Next button
+//Next button function
 const nextButton = document.querySelector("#next");
 
 if (!nextButton) {
   throw new Error("Issues with selector");
 }
 
-
 const nextQuestion = () => {
   //Timer reset
-    counter = 15;
-    timeStop = false;
-    countDown();
+  counter = 15;
+  timeStop = false;
+  countDown();
   // Question display
-    questionIndex++;
-    if(questionIndex < questionsArr.length){
+  questionIndex++;
+  if (questionIndex < questionsArr.length) {
     questionCategory.innerText = `${questionsArr[questionIndex].category}`;
     question.innerText = `${questionsArr[questionIndex].question}`;
     let currentQuestion = questionsArr[questionIndex].answers;
@@ -166,30 +154,42 @@ const nextQuestion = () => {
       answer.innerText = `${currentQuestion[i].answer}`;
       answer.disabled = false;
     });
- 
   } else {
-  alert("End of Quiz!");
-  timeStop = true;
-  return;
-  } 
-}
-nextButton.addEventListener("click", nextQuestion);
+    alert("End of Quiz!");
+    timeStop = true;
+    clearInterval(timer);
+    return;
+  }
+};
 
-//Home button
 
+//Home button function
 const restartQuiz = () => {
   //Timer reset
+  counter = 0;
   timeStop = true;
-  counter = 15;
+  clearInterval(timer);
+  
   //Reset questions and styling
   startPage.style.display = "flex";
   questionPage.style.display = "none";
   playerScoreInput.innerText = `Player score: `;
+  questionsArr = [];
   answers.forEach((answer) => {
     answer.disabled = false;
+    console.log(answer.disabled);
     answer.style.backgroundColor = `#FFF275`;
   });
-}
+  console.log(timer);
+  console.log(timeStop);
+  console.log(counter);
+  return;
+};
 
+
+//Global event Listeners
+startButton.addEventListener("click", chooseCategory);
+grammarCategory.addEventListener("click", startQuiz);
+mathsCategory.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", nextQuestion);
 homeButton.addEventListener("click", restartQuiz);
-
