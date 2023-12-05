28225292a5325827e4b51512b322d5d39032df60
sachie5 <sachamccormack@gmail.com>
2023-12-05 14:04:20 +0000

    next questions generating and correctness

--- src/main.ts
+++ src/main.ts
@@ -1,94 +1,110 @@
 import "./styles/main.css";
-import { CorrectIncorrect, quizQuestions } from "./quizData";
+import { quizQuestions } from "./quizData";
 
-const quiz = document.querySelector<HTMLElement>("main");
+const startPage = document.querySelector<HTMLElement>(".quiz__container");
 const startButton = document.querySelector<HTMLButtonElement>("#start");
 const playerScoreInput =
   document.querySelector<HTMLHeadingElement>(".header__player");
+const questionPage =
+  document.querySelector<HTMLDivElement>(".pages__container");
+const questionCategory = document.querySelector<HTMLHeadingElement>(
+  ".questions--category"
+);
+const question = document.querySelector<HTMLHeadingElement>(
+  ".questions--question"
+);
+const answers = document.querySelectorAll<HTMLButtonElement>(
+  ".answers__container--options"
+);
 
-if (!startButton || !quiz || !playerScoreInput) {
+if (
+  !startButton ||
+  !startPage ||
+  !playerScoreInput ||
+  !questionPage ||
+  !questionCategory ||
+  !question ||
+  !answers
+) {
   throw new Error("Issue with selector");
 }
+//Player Count
+let score = 0;
+let questionIndex = 0;
 
+//Start Quiz
 const startQuiz = () => {
-  //Player Count
-  let score = 0;
-  let questionID = 0;
   playerScoreInput.innerHTML += `<span class= score> ${score}</span>`;
-  //Question 1
-  for (let index = 0; index < quizQuestions.length; index++) {
-    questionID = quizQuestions[index].ID;
-    quiz.innerHTML = `
-    <div class=quiz__container>
-    <div class=questions>
-    <h2 class=questions--category>${quizQuestions[index].category}</h2>
-        <h3 class=questions--question>${quizQuestions[index].question}</h3>
-        </div>
-        <div class= answers__container>
-        <button class=answers__container--options answers__container--option1>${quizQuestions[index].answers[0].answer}</button>
-      <button class=answers__container--options answers__container--option2>${quizQuestions[index].answers[1].answer}</button>
-      <button class=answers__container--options answers__container--option3>${quizQuestions[index].answers[2].answer}</button>
-      <button class=answers__container--options answers__container--option4>${quizQuestions[index].answers[3].answer}</button>
-        </div>
-        <button class=quiz__container--button id = next>Next Question</button>
-        </div>`;
-  }
 
-  const options = document.querySelectorAll<HTMLButtonElement>(
-    ".answers__container--options"
-  );
-  const nextButton = document.querySelector<HTMLButtonElement>(
-    ".quiz__container--button"
-  );
-  const runningScore = document.querySelector<HTMLSpanElement>(".score");
-  const quizContainer =
-  document.querySelector<HTMLDivElement>(".quiz__container");
+  //Filling question correctly
+  startPage.style.display = "none";
+  questionPage.style.display = "flex";
 
-  if (!options || !nextButton || !runningScore|| !quizContainer) {
-    throw new Error("Issue with selector");
-  }
+  questionCategory.innerText = `${quizQuestions[questionIndex].category}`;
+  question.innerText = `${quizQuestions[questionIndex].question}`;
+  let currentQuestion = quizQuestions[questionIndex].answers;
 
-//Correct answer/Incorrect Answer
-  options.forEach((choice) => {
-    choice.addEventListener("click", () => {
-      for (let index = 0; index < quizQuestions.length; index++) {
-        quizQuestions[index].answers.map((option) => {
-          if (option.answer === choice.innerText) {
-            if (option.correctIncorrect === CorrectIncorrect.CORRECT) {
-              choice.style.backgroundColor = "green";
-              runningScore.innerText = ` ${(score += 1)}`;
-              choice.disabled = true;
-            } else {
-              choice.style.backgroundColor = "red";
-              runningScore.innerText = ` ${(score += 0)}`;
-              choice.disabled = true;
-            }
-          }
-        });
-      }
-    });
+  answers.forEach((answer, i) => {
+    answer.innerText = `${currentQuestion[i].answer}`;
   });
-
-  //Next Question
-  nextButton.addEventListener("click", () => {
-    for (let index = 1; index < quizQuestions.length; index++) {
-      if(quizQuestions[index].ID !== questionID ){
-      quizContainer.innerHTML = `
-      <h2 class=quiz__container--category>${quizQuestions[index].category}</h2>
-      <h3 class=quiz__container--question>${quizQuestions[index].question}</h3>
-      <div class= answers__container>
-      <button class=answers__container--options answers__container--option1>${quizQuestions[index].answers[0].answer}</button>
-      <button class=answers__container--options answers__container--option2>${quizQuestions[index].answers[1].answer}</button>
-      <button class=answers__container--options answers__container--option3>${quizQuestions[index].answers[2].answer}</button>
-      <button class=answers__container--options answers__container--option4>${quizQuestions[index].answers[3].answer}</button>
-      </div>
-      <button class=quiz__container--button id = next>Next Question</button>`;
-    }
-    return;
-  }
-  });
+  console.log(questionIndex);
 };
 
 startButton.addEventListener("click", startQuiz);
 
+//Checking answers
+const checkAnswers = (event: Event) => {
+  const buttonClicked = event.target as HTMLButtonElement;
+  let answersArray = quizQuestions[questionIndex].answers;
+   for (let index = 0; index < answersArray.length; index++) {
+      if (buttonClicked.innerText === answersArray[index].answer) {
+        if(answersArray[index].correctIncorrect === "Correct"){
+      console.log(answersArray[index].correctIncorrect);
+      buttonClicked.style.backgroundColor = "green";
+      playerScoreInput.innerText = ` ${(score += 1)}`;
+    } else {
+      buttonClicked.style.backgroundColor = "red";
+    }
+  }
+}
+}
 
+answers.forEach((answer) => {
+  answer.addEventListener("click", checkAnswers);
+});
+
+//Next button
+const nextButton = document.querySelector("#next");
+
+if (!nextButton) {
+  throw new Error("Issues with selector");
+}
+
+
+const nextQuestion = () => {
+    questionIndex++;
+    if(questionIndex < quizQuestions.length){
+    questionCategory.innerText = `${quizQuestions[questionIndex].category}`;
+    question.innerText = `${quizQuestions[questionIndex].question}`;
+    let currentQuestion = quizQuestions[questionIndex].answers;
+
+    answers.forEach((answer, i) => {
+      answer.style.backgroundColor = `#FFF275`;
+      answer.innerText = `${currentQuestion[i].answer}`;
+    });
+  } else {
+  console.log("End of Quiz!");
+  return;
+  } 
+}
+  
+
+  
+ 
+
+
+
+;
+
+
+nextButton.addEventListener("click", nextQuestion);
--- src/quizData.ts
+++ src/quizData.ts
@@ -7,10 +7,10 @@ export type Quiz = {
 
 type Answers = { answer: string; correctIncorrect: string };
 
-export enum CorrectIncorrect {
+/* export enum CorrectIncorrect {
   CORRECT = "correct",
   INCORRECT = "incorrect",
-}
+} */
 
 export const quizQuestions: Quiz[] = [
   {
@@ -18,10 +18,10 @@ export const quizQuestions: Quiz[] = [
     category: "Grammar",
     question: "Which of these is a noun?",
     answers: [
-      { answer: "throw", correctIncorrect: CorrectIncorrect.INCORRECT },
-      { answer: "happiness", correctIncorrect: CorrectIncorrect.CORRECT },
-      { answer: "happily", correctIncorrect: CorrectIncorrect.INCORRECT },
-      { answer: "beautiful", correctIncorrect: CorrectIncorrect.INCORRECT },
+      { answer: "throw", correctIncorrect: "Incorrect" },
+      { answer: "happiness", correctIncorrect: "Correct" },
+      { answer: "happily", correctIncorrect: "Incorrect" },
+      { answer: "beautiful", correctIncorrect: "Incorrect" },
     ],
   },
   {
@@ -30,10 +30,10 @@ export const quizQuestions: Quiz[] = [
     question:
       "Choose the correct conjunction: _____ I am short, I can reach the top of my wardrobe.",
     answers: [
-      { answer: "although", correctIncorrect: CorrectIncorrect.CORRECT },
-      { answer: "until", correctIncorrect: CorrectIncorrect.INCORRECT },
-      { answer: "because", correctIncorrect: CorrectIncorrect.INCORRECT },
-      { answer: "but", correctIncorrect: CorrectIncorrect.INCORRECT },
+      { answer: "although", correctIncorrect: "Correct" },
+      { answer: "until", correctIncorrect: "Incorrect" },
+      { answer: "because", correctIncorrect: "Incorrect" },
+      { answer: "but", correctIncorrect: "Incorrect" },
     ],
   },
   {
@@ -43,16 +43,16 @@ export const quizQuestions: Quiz[] = [
     answers: [
       {
         answer: "name, place or thing",
-        correctIncorrect: CorrectIncorrect.INCORRECT,
+        correctIncorrect: "Incorrect",
       },
       {
         answer: "describes a noun",
-        correctIncorrect: CorrectIncorrect.INCORRECT,
+        correctIncorrect: "Incorrect",
       },
-      { answer: "an action", correctIncorrect: CorrectIncorrect.CORRECT },
+      { answer: "an action", correctIncorrect: "Correct" },
       {
         answer: "describes a verb",
-        correctIncorrect: CorrectIncorrect.INCORRECT,
+        correctIncorrect: "Incorrect",
       },
     ],
   },
@@ -63,19 +63,19 @@ export const quizQuestions: Quiz[] = [
     answers: [
       {
         answer: "I just love doughnuts.",
-        correctIncorrect: CorrectIncorrect.INCORRECT,
+        correctIncorrect: "Incorrect",
       },
       {
         answer: "Will you be coming round for the tea later?",
-        correctIncorrect: CorrectIncorrect.INCORRECT,
+        correctIncorrect: "Incorrect",
       },
       {
         answer: "Pick up the cup.",
-        correctIncorrect: CorrectIncorrect.CORRECT,
+        correctIncorrect: "Correct",
       },
       {
         answer: "London is the south of London.",
-        correctIncorrect: CorrectIncorrect.INCORRECT,
+        correctIncorrect: "Incorrect",
       },
     ],
   },
@@ -84,10 +84,10 @@ export const quizQuestions: Quiz[] = [
     category: "Grammar",
     question: "Which is a synonym of the word 'depressing'?",
     answers: [
-      { answer: "decreasing", correctIncorrect: CorrectIncorrect.INCORRECT },
-      { answer: "gloomy", correctIncorrect: CorrectIncorrect.CORRECT },
-      { answer: "lowering", correctIncorrect: CorrectIncorrect.INCORRECT },
-      { answer: "pushing", correctIncorrect: CorrectIncorrect.INCORRECT },
+      { answer: "decreasing", correctIncorrect: "Incorrect" },
+      { answer: "gloomy", correctIncorrect: "Correct" },
+      { answer: "lowering", correctIncorrect: "Incorrect" },
+      { answer: "pushing", correctIncorrect: "Incorrect" },
     ],
   },
 ];
